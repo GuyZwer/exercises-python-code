add2130310234517e713d437ed6ddd08388beccd
Guy Zwerdling <guy.zwerdling@gmail.com>
2016-05-17 23:21:06 +0300

    no idea code

--- PycharmProjects/HackerRank/No Idea!.py
+++ PycharmProjects/HackerRank/No Idea!.py
@@ -1,10 +1,12 @@
+'''
 happiness = 0
-integers = map(int, raw_input().split())
-elements = map(int, raw_input().split())
+integers = map(int, raw_input().split(' '))
+elements = map(str, raw_input().split(' '))
 numbers = []
-for num in range(integers[1]):
-    numbers.append(map(int, raw_input().split()))
+for num in range(2):
+    numbers.append(map(str, raw_input().split(' ')))
 
+print numbers
 integersA = numbers[0]
 integersB = numbers[1]
 
@@ -17,7 +19,13 @@ for int2 in integersB:
 
 
 print happiness
-
+'''
+n, m = raw_input().split()
+arr = [int(x) for x in raw_input().split()]
+A = set([int(y) for y in raw_input().split()])
+B = set([int(z) for z in raw_input().split()])
+count = [0 + 1 if x in A else 0-1 if x in B else 0 + 0 for x in arr]
+print(sum(count))
 
 
 
