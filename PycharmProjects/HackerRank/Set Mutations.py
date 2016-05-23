f43ce584051e20540aaa2b8fe19b44ea6521bb46
Guy Zwerdling <Guy.Zwerdling@gmail.com>
2016-05-23 20:20:03 +0300

    new file new code

---
+++ PycharmProjects/HackerRank/Set Mutations.py
@@ -0,0 +1,18 @@
+N1 = int(raw_input())
+first_set = set(map(int, raw_input().split()))
+
+def final_command(command):
+    return command
+
+N2 = int(raw_input())
+for loop1 in range(N2):
+    list_for_command = map(str, raw_input().split())
+    command = list_for_command[0]
+    print command
+    set_of_num = set(map(int, raw_input().split()))
+    H = first_set
+    D = set_of_num
+    H.list_for_command[0](D)
+    print H
+
+print H
